b2c8f674fa724b5b28770bd9880282d92c05ea90
Kangming Deng <dengkangming@gmail.com>
2020-11-23 11:25:33 -0500

    Fix undefined render

--- client/src/components/protected/user.js
+++ client/src/components/protected/user.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 
 export default function User(props) {
+  const userDNE = 'CityChat User';
+
+  const renderUser = () => (
+    <Link to={'/app/user/' + props.user.id}>
+        {props.user.name}
+      </Link>
+  );
+
   return (
     <p className="line-height--1 zero--margin">
-      <Link to={'/app/user/' + props.user.id}>
-        {props.user.name || 'CityChat User'}
-      </Link>
+      {props.user ? renderUser() : userDNE}
     </p>
   )
 }
--- server/citychat_server/routes/__init__.py
+++ server/citychat_server/routes/__init__.py
@@ -6,13 +6,64 @@ cors = CORS(supports_credentials=True)
 jwt = JWTManager()
 
 
+def jwt_error_response(f):
+    def wrapper(*args, **kwargs):
+        message = f(*args, **kwargs)
+
+        if isinstance(message, dict):
+            return jsonify(jwt={'status': 401} | message), 401
+        else:
+            return jsonify(jwt={
+                'status': 401,
+                'message': message
+            }), 401
+
+    return wrapper
+
+
+@jwt.claims_verification_failed_loader
+@jwt_error_response
+def claims_verification_failed_loader(callback):
+    return callback
+
+
 @jwt.expired_token_loader
-def my_expired_token_callback(expired_token):
-    expired_access = expired_token['type'] == 'access'
-    return jsonify(jwt={
-        'status': 401,
+@jwt_error_response
+def expired_token_callback(expired_token):
+    expired_access_token = expired_token['type'] == 'access'
+    return {
         'expired': {
-            'access': expired_access,
-            'refresh': not expired_access
+            'access_token': expired_access_token,
+            'refresh_token': not expired_access_token
         }
-    }), 401
+    }
+
+
+@jwt.invalid_token_loader
+@jwt_error_response
+def invalid_token_loader(callback):
+    return callback
+
+
+@jwt.needs_fresh_token_loader
+@jwt_error_response
+def needs_fresh_token_loader(callback):
+    return callback
+
+
+@jwt.revoked_token_loader
+@jwt_error_response
+def revoked_token_loader(callback):
+    return callback
+
+
+@jwt.unauthorized_loader
+@jwt_error_response
+def unauthorized_loader_callback(callback):
+    return callback
+
+
+@jwt.user_loader_error_loader
+@jwt_error_response
+def user_loader_error_loader(callback):
+    return callback
